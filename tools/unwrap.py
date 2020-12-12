ee41b2b0607c50aac397f943230aa8fb046492a4
charles9li <30397709+charles9li@users.noreply.github.com>
2020-12-11 21:17:49 -0800

    added comments, optional parameter to save traj

--- tools/unwrap.py
+++ tools/unwrap.py
@@ -4,21 +4,44 @@ import numpy as np
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Unwraps trajectory.")
     parser.add_argument('traj', type=str, help="trajectory file")
     parser.add_argument('top', type=str, help="topology file")
     parser.add_argument('--method', default='nvt', type=str, help="method of unwrapping")
     return parser.parse_args()
 
 
-def unwrap_traj(traj, top, method='npt'):
+def unwrap_traj(traj_filename, top_filename, method='npt', save_traj=False):
+    """Unwraps a trajectory.
+
+    Reference
+    ---------
+    von Bülow, S.; Bullerjahn, J. T.; Hummer, G. Systematic Errors in Diffusion
+    Coefficients from Long-Time Molecular Dynamics Simulations at Constant Pressure.
+    J. Chem. Phys. 2020, 153 (2), 021101. https://doi.org/10.1063/5.0008316.
+
+    Parameters
+    ----------
+    traj_filename : str
+        Path of trajectory file
+    top_filename : str
+        Path of topology file
+    method : str, optional
+        Which algorithm to use to unwrap trajectories. If 'nvt', the
+        conventional unwrapping scheme is used, and if 'npt, the scheme
+        outlined in the reference
+    save_traj : bool, optional
+        If True, saves the unwrapped trajectory to a dcd file with '_uw'
+        appended to the file prefix. For example, if traj_filename is
+        'test.dcd', the unwrapped trajectory will be saved to 'test_uw.dcd'
+    """
 
     # import wrapped trajectory
-    traj_w = md.load(traj, top=top)
+    traj_w = md.load(traj_filename, top=top_filename)
     xyz_w = traj_w.xyz
 
     # get prefix from traj filename
-    prefix = '.'.join(traj.split('.')[:-1])
+    prefix = '.'.join(traj_filename.split('.')[:-1])
 
     # initialize unwrapped positions
     xyz_uw = np.empty([traj_w.n_frames, traj_w.topology.n_atoms, 3])
@@ -45,7 +68,8 @@ def unwrap_traj(traj, top, method='npt'):
                             unitcell_lengths=traj_w.unitcell_lengths, unitcell_angles=traj_w.unitcell_angles)
 
     # save unwrapped trajectory to file
-    traj_uw.save("{}_uw.dcd".format(prefix))
+    if save_traj:
+        traj_uw.save("{}_uw.dcd".format(prefix))
 
     # return unwrapped trajectory
     return traj_uw
@@ -53,4 +77,4 @@ def unwrap_traj(traj, top, method='npt'):
 
 if __name__ == '__main__':
     args = parse_arguments()
-    unwrap_traj(args.traj, args.top, method=args.method)
+    unwrap_traj(args.traj, args.top, method=args.method, save_traj=True)
