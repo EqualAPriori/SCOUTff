6fa28e5b20423f59b1f573eab8b37a7429aeb19a
Kevin Shen <kshen@pod-login1.podcluster>
2021-06-11 15:50:01 -0700

    fixed spacing typo

--- tools/setup_standard.sh
+++ tools/setup_standard.sh
@@ -33,9 +33,9 @@ cd $mydir
     #frequencies and times below are in terms of picoseconds
     DCDFREQ=100
     THERMOFREQ=10
-    ANNEALTIME=1000
-    EQTIME=1000
-    PRODUCTIONTIME=500000
+    ANNEALTIME=10000
+    EQTIME=10000
+    PRODUCTIONTIME=1000000
 
 
     #set up script
@@ -54,8 +54,8 @@ cd $mydir
     sed -e "s/__label__/$mydir/" < ${toollib}podsubmit_template.sh > ./podsubmit.sh
 
     echo "# Suggested commands" > z.commands
-    echo "python ${toollib}make_topology.py -L $L -pkml $pkmldir -structlib $structlib -m $polymer $npoly -ff ${fflib}TrappeUA_Master.xml" -xyz >> z.commands
-    echo "srun --gres=gpu:1" "python ${toollib}make_topology.py -L $L -pkml $pkmldir -structlib $structlib -m $polymer $npoly -ff ${fflib}TrappeUA_Master.xml" -xyz >> podsubmit.sh
+    echo "python ${toollib}make_topology.py -L $L -pkml $pkmldir -structlib $structlib -m $polymer $npoly -ff ${fflib}TrappeUA_Master.xml" -xyz -PME -tail >> z.commands
+    echo "srun --gres=gpu:1" "python ${toollib}make_topology.py -L $L -pkml $pkmldir -structlib $structlib -m $polymer $npoly -ff ${fflib}TrappeUA_Master.xml" -xyz -PME -tail >> podsubmit.sh
     echo "" >> z.commands
     echo "python ./run.py -ff ${fflib}TrappeUA_Master.xml -PME -tail" >> z.commands
     echo "srun --gres=gpu:1" "python ./run.py -ff ${fflib}TrappeUA_Master.xml -PME -tail" >> podsubmit.sh
